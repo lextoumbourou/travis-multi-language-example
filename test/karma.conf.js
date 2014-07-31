7a87b36db420b03217c3134d078464ee9555c265
Lex Toumbourou <lextoumbourou@gmail.com>
2014-07-31 11:40:14 +1000

    Added nose + karma + travis conf

---
+++ test/karma.conf.js
@@ -0,0 +1,22 @@
+module.exports = function(config) {
+  config.set({
+    basePath: '../',
+    frameworks: ['jasmine'],
+    files: [
+      'app/js/*.js',
+      'test/js/*.js'
+    ],
+    exclude: [],
+    preprocessors: {'app/js/app.js': 'coverage'},
+    reporters: ['progress', 'coverage'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['PhantomJS'],
+    singleRun: false,
+    coverageReporter: {
+      type : 'text',
+    }
+  });
+};
